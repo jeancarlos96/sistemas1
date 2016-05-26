16b5f8e0115fd724447cfe8d31910df3ab526b64
jeancarlos96 <jeancarloschamba@hotmail.com>
2016-05-26 08:56:28 -0500

    impresion del valor de entrada

--- pago.py
+++ pago.py
@@ -1,7 +1,8 @@
-x = int(input("ingrese el valor de la cena a comer : "))
-def pago():
-	a = (x * 12)/100
-	print (a)
-	return x + a
-calculo = pago()
-print (calculo)
+x = int(input("ingrese el valor de la cena a comer : ")) ##INGRESO DE DATOS
+def pago():  ## DEFINICION DE FUNCION
+	a = (x * 12)/100  ## SACAR EL PORCENTAJE DEL TOTAL
+	print (a) ## IMPRIMIMOS EL TANTO POR CIENTO 
+	return x + a ## RETORNO DEL RESULTADO 
+calculo = pago() ## LLAMANDO A LA FUNCION
+print (calculo) ## IMPRIMIMOS EL RESULTADO FINAL
+print (x)
